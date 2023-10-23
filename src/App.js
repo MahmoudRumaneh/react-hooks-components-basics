8cd151a2f3599dc04f0f90a09c357a5e34ef45e7
MahmoudRumaneh <mahmoudrmn@gmail.com>
2023-10-23 10:13:38 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+
 function App() {
   return (
     <div>
-      <Article />
+      <Navbar />
+      <Home />
+      <About />
       <Comment />
+    </div>
+  );
+}
+function Navbar() {
+  return (
+    <div id="navbar">
+      <h2><u>I'm a link</u></h2>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
     </div>
   );
 }
